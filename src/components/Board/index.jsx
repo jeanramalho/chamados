0482345fba23e6604d191fd15e923ca4a8a26b9a
jeanramalho <jeanramalho.dev@gmail.com>
2022-09-05 13:31:36 -0300

    Cria quadro para exibição do chamado

--- src/components/Board/index.jsx
+++ src/components/Board/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import List from '../List';
+import Ticket from '../Ticket';
 import { Container } from './style';
 
 function Board() {
   return (
     <Container>
         <List />
+        <Ticket />
     </Container>
   );
 }
---
+++ src/components/Ticket/style.jsx
@@ -0,0 +1,68 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    width: 87%;
+    height: 100%;
+    padding: 15px;
+    
+
+    .headerInfo {
+
+        margin-bottom: 30px;
+
+        h1 {
+            text-transform: uppercase;
+            font-size: 2rem;
+        };
+
+        h4 {
+            font-size: 1.3rem;
+            font-weight: 600;
+        };
+    };
+
+    .info, .info2 {
+        width: 100%;
+        display: flex;
+        justify-content: space-between;
+        font-size: 12pt;
+        margin-top: 10px;
+    };
+
+    h3 {
+        margin: 30px 0 15px 0;
+    };
+
+    .desc {
+        display: flex;
+        width: 100%;
+        height: 60%;
+        align-items: center;
+        justify-content: center;
+        overflow: hide;
+    }
+
+    .content {
+        border: 2px solid #8888;
+        overflow-y: scroll;
+        width: 95%;
+        height: 100%;
+        border-radius: 8px;
+    }
+
+    .content::-webkit-scrollbar {
+        width: 12px;              
+      }
+      
+      .content::-webkit-scrollbar-track {
+        background: #555;        
+      }
+      
+      .content::-webkit-scrollbar-thumb {
+        background-color: #222;    
+        border-radius: 20px;       
+        border: 3px solid #333;  
+      }
+
+    
+`;
