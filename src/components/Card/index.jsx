03aff8e468e05fff39a52ed85722f65def93ca30
jeanramalho <jeanramalho.dev@gmail.com>
2022-09-05 12:17:26 -0300

    Finaliza estilização do card

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -4,7 +4,7 @@ import { Container } from './style';
 function Card() {
   return (
     <Container>
-        <h3>Nome do Chamdo</h3>
+        <h3>Nome do Chamado</h3>
         <div>
             <p>Nome do Criador</p>
             <span>00/00/0000</span>
--- src/components/Card/style.jsx
+++ src/components/Card/style.jsx
@@ -1,5 +1,21 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  
+    width: 100%;
+    height: 60px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    padding: 10px;
+    border-bottom: 1.5px solid #8888;
+
+
+   div {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-top: 8px;
+   }
 `;
