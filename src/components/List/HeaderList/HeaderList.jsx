9b7c732847cdffd976c98317f28d41b8e4dee427
jeanramalho <jeanramalho.dev@gmail.com>
2022-09-08 14:20:03 -0300

    Implementa link para home no nome chamados no header da lista de chamados

--- src/components/List/HeaderList/HeaderList.jsx
+++ src/components/List/HeaderList/HeaderList.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import  {Container}  from './HeaderListStyle';
+import { Link } from 'react-router-dom';
 
 function HeaderList() {
   return (
         <Container>
-            <h2>chamados</h2>
+            <Link to={`/`} className="link"><h2>chamados</h2></Link>
             <button>Criar</button>
         </Container>        
   );
