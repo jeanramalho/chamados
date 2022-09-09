e31bd3cc851a5509741a5650f8f4c0b19a541c31
jeanramalho <jeanramalho.dev@gmail.com>
2022-09-09 12:58:08 -0300

    inicia a implementação de modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 import React from "react";
-
 import { ApolloProvider } from '@apollo/client'
-
 import { client } from './lib/apollo'
-
 import GlobalStyle from "./styles/global";
-
 import Router from "./components/Router";
-
 import { BrowserRouter } from 'react-router-dom'
 
+
 function App() {
+
   return (
      <ApolloProvider client={client}>
        <BrowserRouter>
