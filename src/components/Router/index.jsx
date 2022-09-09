839c52971123aa91390ee55fe8cd2215577b2c42
jeanramalho <jeanramalho.dev@gmail.com>
2022-09-09 13:27:09 -0300

    Adiciona componentes na rota de criação de chamados

--- src/components/Router/index.jsx
+++ src/components/Router/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom'
 import Home from '../../pages/Home';
 import TicketPages from '../../pages/TicketPages';
+import CreatePage from '../../pages/CreatePage'
 
 
 
@@ -10,6 +11,7 @@ function Router() {
     <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/chamado/:id' element={<TicketPages />} />
+        <Route path='/create' element={<CreatePage />} />
     </Routes>
   );
 }
--- src/pages/CreatePage.jsx
+++ src/pages/CreatePage.jsx
@@ -3,15 +3,14 @@ import { ApolloProvider } from '@apollo/client'
 import { client } from '../lib/apollo'
 import Header from "../components/Header";
 import GlobalStyle from "../styles/global";
-import Board from "../components/Board"
+
 
 
 function TicketPages() {
   return (
      <ApolloProvider client={client}>     
         <Header />
-        <Board />
-        <GlobalStyle />          
+        <GlobalStyle />
     </ ApolloProvider>
     
         
