b9b0cfd9d738c9d30272d0904d62e49f47718f90
jeanramalho <jeanramalho.dev@gmail.com>
2022-09-12 13:19:32 -0300

    Inicia estilização da página de criação de chamado

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -5,10 +5,20 @@ import { Container } from './style';
 function Form() {
   return (
     <Container>
-        <input type="text" placeholder='Seu nome' />
-        <input type="text" placeholder='Título do chamado' />
-        <textarea name="descricao" id="" cols="30" rows="10" placeholder='Descreva o chamado...'></textarea>
-        <button>Enviar</button>        
+       <div className="content">
+          <input type="text" placeholder='Seu nome' />
+          <input type="text" placeholder='Título do chamado' />
+          <textarea name="descricao" id="" cols="30" rows="20" placeholder='Descreva o chamado...'></textarea>
+          <button>Enviar</button>  
+       </div>
+       <div className="infos">
+          <select name="testando" id="test">
+            <option value="nois">teste 1</option>
+            <option value="nois">teste 3</option>
+            <option value="nois">teste 2</option>
+          </select>
+       </div>
+              
     </Container>
   );
 }
--- src/components/Form/style.jsx
+++ src/components/Form/style.jsx
@@ -2,20 +2,37 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
    display: flex;
-   width: 65%;
+   width: 100%;
+   height: calc(100% - 88px);
    align-items: center;
-   flex-direction: column;
-   justify-content: center;
+   flex-direction: row;
+   justify-content: space-evenly;
+
+   .content {
+    width: 70%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+   }
+
+   .infos {
+    width: 30%;
+    height: 100%;
+    background: red;
+   }
 
    input, textarea, button {
      border-radius: 8px;
      width: 90%; 
      margin-top: 10px;   
      padding: 8px; 
+     background: #444;
+     color: #fff;
    };
 
    input {
-    height: 30px;    
+    height: 50px;    
    };
 
    button {
