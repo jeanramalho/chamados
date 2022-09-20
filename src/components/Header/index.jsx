96909a2922b4e8e3f61af6a70fd7214b3705e1fd
jeanramalho <jeanramalho.dev@gmail.com>
2022-09-20 20:45:53 -0300

    Adiciona Link de volta pra home no header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Container } from './style'
+import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
     <Container>
-        <h1>Code 7 Chamados</h1>
+        <Link to="/"><h1>Code 7 Chamados</h1></Link>
     </Container>
   )
 }
