9689c9f42f387044072a293e60bfa127c03a4e1c
jeanramalho <jeanramalho.dev@gmail.com>
2022-09-28 22:43:02 -0300

    Tira o windows location momentaneamente

--- src/components/Ticket/index.jsx
+++ src/components/Ticket/index.jsx
@@ -24,7 +24,7 @@ function Ticket(props) {
             }
         )
 
-        document.location.reload()
+        
         return  alert(`Chamado de numero ${props.taskId} deletado com sucesso!`)
     }
 
