c6afe7153c21b9bd164286d07407c4f90ca08596
jeanramalho <jeanramalho.dev@gmail.com>
2022-10-03 20:01:27 -0300

    Cria input de task id

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -7,6 +7,7 @@ function Modal(){
         <Container>
             <div className="modalForm">
                 <input type="text" id="taskTitle" />
+                <input type="text" id="taskId" />
             </div>
         
         </Container>
