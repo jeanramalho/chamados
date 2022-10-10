fb01da271013ee5e50f14beae9f07285fa401f70
jeanramalho <jeanramalho.dev@gmail.com>
2022-10-10 20:41:39 -0300

    Inicia reparação no estilo do modal

--- src/components/Modal/style.jsx
+++ src/components/Modal/style.jsx
@@ -28,6 +28,7 @@ export const Container = styled.div`
             width: 100%;
             display: flex;
             align-items: center;
+            justify-content: center;
         }
 
         .divContentDois {
