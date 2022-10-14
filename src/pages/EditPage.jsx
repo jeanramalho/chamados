f4da58d315ce7e111e4253a9dec0f34b98b206df
jeanramalho <jeanramalho.dev@gmail.com>
2022-10-14 13:12:11 -0300

    Cria ambiente de edição

--- src/pages/EditPage.jsx
+++ src/pages/EditPage.jsx
@@ -3,18 +3,14 @@ import { ApolloProvider } from '@apollo/client'
 import { client } from '../lib/apollo'
 import Header from "../components/Header";
 import GlobalStyle from "../styles/global";
-import Board from "../components/Board"
 import Modal from "../components/Modal"
-import List from "../components/List";
 
 
 function EditPage() {
   return (
      <ApolloProvider client={client}>     
         <Header />
-          <Board>
-              <Modal />
-          </Board>        
+        <Modal />
         <GlobalStyle />          
     </ ApolloProvider>
     
