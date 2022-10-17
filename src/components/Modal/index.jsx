7d8f8d2ac1838a756e2a4db5ce40f7f02d88565b
jeanramalho <jeanramalho.dev@gmail.com>
2022-10-17 15:12:28 -0300

    Incia reformulação da página de edit

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -9,28 +9,7 @@ function Modal(props){
         <Container>
                 <List />
                <div className="modalForm">
-                    <div className="divContent">
-                        <p className="titleTicket">Título do Chamado</p>
-                        <input type="text" id="taskTitle" className="taskTitle" />
-                    </div>
-
-                    <div className="divContentDois">
-                        <p>ID do Chamado</p>
-                        <input type="text" id="taskId" className="dois"/>
-
-                        <p>Criticidade</p>
-                        <input type="text" id="criticidade" className="dois"/>
-                    </div>
-                    
-                    <div className="divContent tres">
-                        <p className="descTask">Descrição do Chamado</p>
-                        <textarea  id="taskDesc" cols="30" rows="10" className="taskDesc"></textarea>
-                    </div>
-                    
-                    <div className="footer">
-                        <button className="fechar">Fechar</button>
-                        <button className="saveButton">Salvar</button>
-                    </div>
+               
                </div>       
                 
                  
--- src/components/Modal/style.jsx
+++ src/components/Modal/style.jsx
@@ -3,86 +3,8 @@ import styled from "styled-components";
 export const Container = styled.div`
             width: 100%;
             height: calc(100% - 80px);
-            display: flex;
-            flex-direction: row;
-            justify-content: space-evenly;
-            align-items: center;    
+            
                   
 
-        .modalForm {
-            width: 100%;
-            height: 100%;
-            background: #333;
-            border-radius: 8px;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            flex-direction: column;
-        }
-
-        .divContent, .divContentDois {
-            margin-bottom: 10px;
-            width: 100%;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-        }
-
-        .divContentDois {
-            justify-content: space-evenly;
-        }
-
-        .titleTicket {
-            width: 15%;
-            display: flex;
-            justify-content: flex-end;
-            margin-rigth: 10px;
-
-        }
-
-        input {
-            margin: 7px;
-            width: 60%;
-            height: 35px;
-            border-radius: 8px;
-        }
-
-        .taskTitle {
-            width: 65%;
-        }
-
-        .dois {
-            width: 20% !important;
-        }
-
-        .tres {
-            display: flex;
-            flex-direction: column;
-        }
-
-        .taskDesc {
-            width: 70%;
-            heigth: 150px;
-            border-radius: 8px;
-
-        }
-
-        .descTask {
-            width: 100%;
-            display: flex;
-            justify-content: flex-start;
-            margin-left: 130px;
-            margin-bottom: 20px;
-        }
-
-        button {
-            width: 80px;
-            height: 30px;
-            border-radius: 5px;
-            font-size: 12pt;
-            font-weight: 500;
-            text-transform: uppercase;
-            margin: 20px;
-        }
 
 `
