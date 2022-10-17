4ed33d296e1563dc77d3ae9ce50c8ee9980093c5
jeanramalho <jeanramalho.dev@gmail.com>
2022-10-17 17:25:01 -0300

    Consegue ajeitar div do form

--- src/components/Edit/index.jsx
+++ src/components/Edit/index.jsx
@@ -8,7 +8,7 @@ function Edit(props){
     return (
         <Container>
                 <List />
-               <div className="modalForm">
+               <div className="editForm">
                
                </div>       
                 
--- src/components/Edit/style.jsx
+++ src/components/Edit/style.jsx
@@ -3,8 +3,13 @@ import styled from "styled-components";
 export const Container = styled.div`
             width: 100%;
             height: calc(100% - 80px);
+            display: flex;
             
-                  
+             .editForm {
+                width: 100%;
+                height: 100%;
+                background: #333;
+             }     
 
 
 `
