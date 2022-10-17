f2930bd50f9355d7ea437247da883dcbe03db828
jeanramalho <jeanramalho.dev@gmail.com>
2022-10-17 13:24:24 -0300

    Ainda tentando entender o contexto do estilo

--- src/components/Modal/style.jsx
+++ src/components/Modal/style.jsx
@@ -1,15 +1,17 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-            width: 100%;
-            height: 100%;
+            width: 90%;
+            height: calc(100% - 80px);
             display: flex;
-            justify-content: center;
-            align-items: center;            
+            flex-direction: row;
+            justify-content: space-around;
+            align-items: center;    
+                  
 
         .modalForm {
-            width: 80%;
-            height: 80%;
+            width: 100%;
+            height: 100%;
             background: #333;
             border-radius: 8px;
             display: flex;
