dd382d8a3a768c26e1d9684e18e24a83f97183fb
jeanramalho <jeanramalho.dev@gmail.com>
2022-10-27 14:16:27 -0300

    Exporta função de update

--- src/components/Edit/editChamado.jsx
+++ src/components/Edit/editChamado.jsx
@@ -2,6 +2,20 @@ import React from 'react';
 import { useMutation } from '@apollo/client';
 import { updateChamado } from './mutation';
 
-function updateTask() {
+function updateTask(desc, status, title, crit, taskId) {
+    const [upChamado, {}] = useMutation(updateChamado)
     
+        upChamado(
+            {
+                variables: {
+                    description: desc,
+                    statusTicket: status,
+                    title: title,
+                    criticality: crit,
+                    taskId: parseInt(taskId)
+                }
+            }
+        )
 }
+
+export default updateTask
