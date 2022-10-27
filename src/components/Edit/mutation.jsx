e5e86a1e757ad3e75a81f2717c35118a36a9588a
jeanramalho <jeanramalho.dev@gmail.com>
2022-10-27 14:02:24 -0300

    Cria um função de update

--- src/components/Edit/mutation.jsx
+++ src/components/Edit/mutation.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { gql, useMutation, useQuery } from '@apollo/client';
 
-export const deleteChamado = gql`
+export const updateChamado = gql`
     mutation($description: String, $statusTicket: String, $title: String, $criticality: String, $taskId: Int ) {
         updateChamado(
           data: {description: $description, statusTicket: $statusTicket, title: $title, criticality: $criticality}
