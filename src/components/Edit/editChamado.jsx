c731eb760ef0f0e5fce980185b9e2c37b45b9bf3
jeanramalho <jeanramalho.dev@gmail.com>
2022-10-28 17:21:13 -0300

    Cria nova estrategia para ver se o mutation funciona

--- src/components/Edit/editChamado.jsx
+++ src/components/Edit/editChamado.jsx
@@ -1,35 +1,10 @@
 import React from 'react';
-import { useMutation } from '@apollo/client';
+import { gql, useQuery, useMutation } from '@apollo/client';
 import { updateChamado } from './mutation';
 
 
-
-function UpdateTask() {
-
-    const [upChamado, {}] = useMutation(updateChamado)
-    
-    let tit = document.getElementById('title').value
-    let id = document.getElementById('Id').value
-    let desc= document.getElementById('description').value
-    let criti = document.getElementById('crit').value
-    let status = document.getElementById('statusTask').value
+export const [upChamado, {}] = useMutation(updateChamado)
 
 
-    
-        upChamado(
-            {
-                variables: {
-                    description: desc,
-                    statusTicket: status,
-                    title: tit,
-                    criticality: criti,
-                    taskId: parseInt(id)
-                }
-            }
-        )
-    
 
-        return alert(`Chamado ${id} foi atualizado com sucesso!`)
-}
 
-export default UpdateTask
