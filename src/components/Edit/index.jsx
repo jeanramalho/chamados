a0e4b37ec8c35208c5c66de191c83338794b0bab
jeanramalho <jeanramalho.dev@gmail.com>
2022-10-28 17:15:28 -0300

    Coloca tudo no mesmo arquivo

--- src/components/Edit/index.jsx
+++ src/components/Edit/index.jsx
@@ -33,7 +33,33 @@ function Edit(props){
   const chamado = data.chamado
     console.log(chamado.taskId)
 
+    function UpdateTask() {
 
+        const [upChamado, {}] = useMutation(updateChamado)
+        
+        let tit = document.getElementById('title').value
+        let id = document.getElementById('Id').value
+        let desc= document.getElementById('description').value
+        let criti = document.getElementById('crit').value
+        let status = document.getElementById('statusTask').value
+    
+    
+        
+            upChamado(
+                {
+                    variables: {
+                        description: desc,
+                        statusTicket: status,
+                        title: tit,
+                        criticality: criti,
+                        taskId: parseInt(id)
+                    }
+                }
+            )
+        
+    
+            return alert(`Chamado ${id} foi atualizado com sucesso!`)
+    }
 
 
 
