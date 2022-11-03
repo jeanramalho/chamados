733ad49de7bb158f07f12b40155f8c27ac9a2c09
jeanramalho <jeanramalho.dev@gmail.com>
2022-11-02 22:05:49 -0300

    Tela branca ao colocar funcao

--- src/components/Edit/editChamado.jsx
+++ src/components/Edit/editChamado.jsx
@@ -1,30 +1,5 @@
 import React from 'react';
-import { gql, useQuery, useMutation } from '@apollo/client';
-import { updateChamado } from './mutation';
 
-function UpdateTask(){
-
-
-    let tit = document.getElementById('title').value
-    let idChamado = document.getElementById('Id').value
-    let desc= document.getElementById('description').value
-    let criti = document.getElementById('crit').value
-    let status = document.getElementById('statusTask').value
-
-    const [upChamado, {}] = useMutation(updateChamado)
-
-    upChamado(
-        {
-            variables: {
-                description: desc,
-                statusTicket: status,
-                title: tit,
-                criticality: criti,
-                taskId: parseInt(idChamado)
-            }
-        }
-    )
-}
 
 
 
