0660ce746da0e674b19fbf19aa2325173770272c
jeanramalho <jeanramalho.dev@gmail.com>
2022-11-04 16:39:12 -0300

    Salva teste 1

--- src/components/Edit/index.jsx
+++ src/components/Edit/index.jsx
@@ -40,31 +40,7 @@ function Edit(props){
   const chamado = data.chamado
         
   
-  function UpdateTask(){
-
-
-    let tit = document.getElementById('title').value
-    let idChamado = document.getElementById('Id').value
-    let desc= document.getElementById('description').value
-    let criti = document.getElementById('crit').value
-    let status = document.getElementById('statusTask').value
-
-    const [upChamado, {}] = useMutation(updateChamado)
-
-    upChamado(
-        {
-            variables: {
-                description: desc,
-                statusTicket: status,
-                title: tit,
-                criticality: criti,
-                taskId: parseInt(idChamado)
-            }
-        }
-    )
-
-    return alert("Chamado atualizado com sucesso")
-}
+  
         
 
 
